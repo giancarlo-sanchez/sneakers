5fd50d45eac8745d099a222b3516f05716ef08d1
jank_ <jank_329@hotmail.com>
2020-08-07 08:05:44 -0500

    Adding routes

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -4,75 +4,75 @@ import {Link} from 'react-router-dom'
 function LandingPage(props){
     return <div className="landing-page">
         <div>
-            <a href="/catalog">
+            <Link to={`/catalog`}>
                 <div>
                 <img src="https://i.postimg.cc/vmhKLJhH/background.png" alt="catalog"/>
                 </div>
 
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/1">
+            <Link to={`/brands/1`}>
             <div>
                 <img src="https://i.postimg.cc/K8m0Y97P/be2e9523f1c65739f648fcde45b6abec.jpg" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/2">
+            <Link to={`/brands/2`}>
             <div>
                 <img src="https://i.postimg.cc/Y024JWBw/converse2.png" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/3">
+            <Link to={`/brands/3`}>
             <div>
                 <img src="https://i.postimg.cc/D0YzHpcq/newbalance.png" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/4">
+            <Link to={`/brands/4`}>
             <div>
                 <img src="https://i.postimg.cc/Kj9d5d5y/nike-2.png" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/5">
+            <Link to={`/brands/5`}>
             <div>
                 <img src="https://i.postimg.cc/zXsd1zvc/reebok.png" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/5">
+            <Link to={`/brands/6`}>
             <div>
                 <img src="https://i.postimg.cc/nV65nYw2/ce24e13c2d5c73fd4b2fdd3c73e7dc18.jpg" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/6">
+            <Link to={`/brands/7`}>
             <div>
                 <img src="https://i.postimg.cc/s2v9BbMh/19fa1bbd922e59f930f9b4e38c9f7af6.jpg" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/7">
+            <Link to={`/brands/8`}>
             <div>
                 <img src="https://i.postimg.cc/mrsBhWXd/031bccc1ba2c2c50d6de6899ba293005.jpg" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
         <div>
-            <a href="/brands/7">
+            <Link to={`/brands/9`}>
             <div>
                 <img src="https://i.postimg.cc/4x8jRkC7/30-78190.jpg" alt="catalog"/>
             </div>
-            </a>
+            </Link>
         </div>
     </div>
 }
